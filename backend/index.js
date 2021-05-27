218320991a2d404d01f73f07ff2803d6ec0b0ce0
yahya.jideh@gmail.com <yahya.jideh@appiancorp.com>
2021-05-27 16:56:13 +1000

    Added DAO.

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,24 @@ const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
 
-MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI, 
-    {
-        poolSize: 50, 
-        wtimeout: 2500,
-        useNewUrlParser: true
-    }
-)
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-})
-.then(async client => {
-    app.listen(port, ()=> {
-        console.log(`Listening on port ${port}`);
-    })
+// MongoClient.connect(
+//     process.env.RESTREVIEWS_DB_URI, 
+//     {
+//         poolSize: 50, 
+//         wtimeout: 2500,
+//         useNewUrlParser: true
+//     }
+// )
+// .catch(err => {
+//     console.error(err.stack)
+//     process.exit(1)
+// })
+// .then(async client => {
+//     app.listen(port, ()=> {
+//         console.log(`Listening on port ${port}`);
+//     })
+// })
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`);
 })
